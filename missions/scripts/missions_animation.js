54b30248ea73b86f63a7299bb950b2d86c3d78f5
Silent Sonata <musictechy.cb@gmail.com>
2019-02-06 15:53:10 -0500

    Removed Section Links and moved interactive banners up. Modified animations to also complement the change/

--- missions/scripts/missions_animation.js
+++ missions/scripts/missions_animation.js
@@ -1,14 +1,8 @@
-var debug = document.getElementById('debug');
-var sectionOne = document.getElementById('section1');
 var sectionTwo = document.getElementById('section2');
 var sectionThree = document.getElementById('section3');
 
 function scrolling() {
     var windowBottom = window.scrollY + (window.innerHeight / 2);
-    if (sectionOne.offsetTop <= windowBottom) {
-        sectionOne.classList.add('text-animation');
-        sectionOne.classList.add('background-animation');
-    }
 
     if (sectionTwo.offsetTop <= windowBottom) {
         sectionTwo.classList.add('text-animation');
@@ -19,6 +13,4 @@ function scrolling() {
         sectionThree.classList.add('text-animation');
         sectionThree.classList.add('background-animation');
     }
-
-    debug.innerHTML = windowBottom;
 }
