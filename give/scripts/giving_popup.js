9f3cb649f28aa3d761dff21be345a634f7e4f58e
Silent Sonata <musictechy.cb@gmail.com>
2019-02-21 16:42:27 -0500

    Created popup

---
+++ give/scripts/giving_popup.js
@@ -0,0 +1,9 @@
+var popupWindow = document.getElementById('text-overlay');
+
+function closePopup() {
+    popupWindow.style.display = "none";
+}
+
+function openPopup() {
+    popupWindow.style.display = "inline";
+}
