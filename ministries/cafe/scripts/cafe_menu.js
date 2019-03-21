9a186c8e25bcf59ae001d518103e635e4f9002a1
Silent Sonata <musictechy.cb@gmail.com>
2019-03-21 10:47:06 -0400

    Implemented Firebase

---
+++ ministries/cafe/scripts/cafe_menu.js
@@ -0,0 +1,25 @@
+// Imports from HTML doc
+const wednesdayDate = document.querySelector("#wednesday-menu-date");
+const wednesdayMenu = document.querySelector("#wednesday-menu");
+const saturdayDate = document.querySelector("#saturday-menu-date");
+const saturdayMenu = document.querySelector("#saturday-menu");
+
+// Reference where the data is stored in the database
+const wednesdayDocRef = firestore.doc("cafe-menu/wednesday");
+const saturdayDocRef = firestore.doc("cafe-menu/saturday");
+
+getRealtimeUpdate();
+
+function getRealtimeUpdate() {
+    wednesdayDocRef.onSnapshot(function (doc) {
+        const fetchedData = doc.data();
+        wednesdayDate.innerHTML = fetchedData.date;
+        wednesdayMenu.innerHTML = fetchedData.menu;
+    });
+
+    saturdayDocRef.onSnapshot(function (doc) {
+        const fetchedData = doc.data();
+        saturdayDate.innerHTML = fetchedData.date;
+        saturdayMenu.innerHTML = fetchedData.menu;
+    });
+}
