b3d0bbde18bab84c14b1368dfb508bc7d5b2a3b6
Silent Sonata <musictechy.cb@gmail.com>
2019-05-01 15:54:08 -0400

    Reformated so expireIt actually removes the element vs just hidding it

--- scripts/expire_it.js
+++ scripts/expire_it.js
@@ -5,7 +5,7 @@
 
     Classes:
     'expires' (Required) - Put on parent element that will hide if expired.
-    'expire-date' (Required) - On each element that will expire. FORMAT: <YEAR>-<MONTH>-<DAY> 2019-04-12
+    'expire-date' (Required) - On each element that will expire. Element will expire the day after entered date. FORMAT: <YEAR>-<MONTH>-<DAY> 2019-04-12
     'expire-empty' (Optional) - An element that will notify user that everything is expired. Will be displayed inline.
 
     1. Add function expireIt() to body onload.
@@ -17,39 +17,42 @@
         <div class="expire-date">2019-02-11</div>
         Content goes here...
     </div>
+
+    BANNER USE:
+    In order to use on banners:
+    1. Make sure expireIt() is loaded before startSlideShow() in onload body section.
+    2. Wrap entire slide in a hold div:
+    <div class="expires">
+        <div class="expire-date"></div>
+        <div id="slide1">
+            Slide Content ...
+        </div>
+    </div>
  */
 
-
-// Capture all elements that can expire
-var elements = document.querySelectorAll(".expires");
+// Capture all expireElements that can expire
+var expireElements = document.querySelectorAll(".expires");
 var expireDates = document.querySelectorAll(".expire-date");
 var expireEmpty = document.querySelector("#expire-empty")
 
-var TAG = "EXPIRE IT: "
 
 /* Checks to see if any element on the page expired. Must be loaded on page load. */
 function expireIt() {
+    var TAG = "expireIt(): "
     var currentTime = new Date().getTime();
     var currentIndex = 0;
     var expiredItems = 0;
 
-    console.log(expireDates);
-
     expireDates.forEach((element) => {
         // Format for the Date class
-        
         var dateCaptured = new Date(element.innerHTML).getTime();
-
-
         // Add number of milliseconds in a day to show element on expiration date
         dateCaptured += 86400000;
-
-        console.log(dateCaptured); 
 
         if ((dateCaptured) < currentTime) {
             // Element expired
             expiredItems++;
-            elements[currentIndex].style.display = 'none';
+            expireElements[currentIndex].remove(expireElements[currentIndex]);
         }
         currentIndex++;
     });
@@ -63,7 +66,7 @@ function expireIt() {
 
     // Check if all items are expired
     if (expireEmpty != null) {
-        if (expiredItems == elements.length) {
+        if (expiredItems == expireElements.length) {
             expireEmpty.style.display = 'inline';
         }
     }
