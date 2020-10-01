3ab1a6ed073e7ef2f50aa3028f09ef7f6f5e2861
Silent Sonata <musictechy.cb@gmail.com>
2020-10-01 12:15:57 -0400

    Added sermon notes to the live page

--- live/index.php
+++ live/index.php
@@ -114,8 +114,18 @@ function getLiveVideoID($channelId)
             <div id="clock"></div>
         </div>
         <div id="button-group">
+            <!-- Give -->
             <a href="/give/" target="_blank" class="button--white">Give Online</a>
-            <a href="/events/" target="_blank" class="button--white">Our Events</a>
+            <!-- Sermon Notes -->
+            <div class="expires">
+                <div class="expire-date">2020-10-03</div>
+                <a class="button--white" href="/seriesmatterial/200913_Notes.pdf">Notes</a>
+            </div>
+            <div class="hold">
+                <div class="hold-date">2020-10-03</div>
+                <a class="button--white" href="/seriesmatterial/201003_Notes.pdf">Sermon Notes</a>
+            </div>
+            <!-- About Us -->
             <a href="/about/" target="_blank" class="button--white">About Us</a>
         </div>
     </div>
