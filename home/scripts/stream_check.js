976bb4dcc3fae62ab03bc05f5dd7fd4e4297d277
Silent Sonata <musictechy.cb@gmail.com>
2020-11-12 09:41:56 -0500

    Reworked live streaming system

--- home/scripts/stream_check.js
+++ home/scripts/stream_check.js
@@ -7,8 +7,12 @@ var sundayStartS1 = [8, 59];
 var sundayEndS1 = [10, 10];
 var sundayStartS2 = [10, 59];
 var sundayEndS2 = [12, 10];
-var sundayStartS3 = [17, 59];
-var sundayEndS3 = [19, 10];
+
+// Tool Tip code
+var date = new Date();
+var toolTipStart = [11, 59];
+var toolTipEnd = [12, 30];
+toolTipCheck(date, toolTipStart, toolTipEnd);
 
 // Run checker
 liveTrigger();
@@ -71,5 +75,60 @@ function checkLive(currentDate, startTime, endTime, day) {
 
     if (goLive) {
         window.location.replace('/live/');
+    }
+}
+
+function toolTipCheck(currentDate, startTime, endTime, day) {
+
+    // TODO: Add the prayer TV logic here
+
+    // Should go live
+    var goLive = false;
+    var shouldCheckDay = false;
+    var cStartTime, cEndTime, cCurrentTime;
+
+    // Convert current date
+    var currentHour = currentDate.getUTCHours();
+    var currentDay = currentDate.getUTCDay();
+    var currentMin = currentDate.getUTCMinutes();
+
+    // Convert hour of start and end to UTC
+    var startHourTimeUTC = startTime[0] + 4;
+    var endHourTimeUTC = endTime[0] + 4;
+
+    // Check if day should be calculated
+    if (day != null) {
+        shouldCheckDay = true;
+    }
+
+    // Convert timing into minutes
+    cStartTime = (startHourTimeUTC * 60) + startTime[1];
+    cEndTime = (endHourTimeUTC * 60) + endTime[1];
+    cCurrentTime = (currentHour * 60) + currentMin;
+
+    // Check if should go live
+    if (shouldCheckDay) {
+        if (currentDay == day) {
+            if (cCurrentTime >= cStartTime) {
+                if (cCurrentTime <= cEndTime) {
+                    goLive = true;
+                }
+            }
+        }
+    } else {
+        if (cCurrentTime >= cStartTime) {
+            if (cCurrentTime <= cEndTime) {
+                goLive = true;
+            }
+        }
+    }
+
+    if (goLive) {
+        var toolTipElement = document.querySelector('#banner-buttons #tool-tip');
+        toolTipElement.classList.add('tool-tip-in');
+
+        // Reroute live button to prayer tv
+        var liveButton = document.getElementById("live-button");
+        liveButton.setAttribute('href', '/live/prayertv/');
     }
 }
